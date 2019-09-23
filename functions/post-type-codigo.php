cf14ea585cc0ecb9a8fc5483a30d029401ace1ae
Jaime de Greiff <alegreiff@gmail.com>
2019-09-22 20:04:33 -0500

    CPT Código

--- functions/post-type-codigo.php
+++ functions/post-type-codigo.php
@@ -1,47 +1,45 @@
 <?php
-
 // Register Custom Post Type
-function custom_post_type() {
+function crea_codigo() {
 
 	$labels = array(
-		'name'                  => _x( 'Post Types', 'Post Type General Name', 'text_domain' ),
-		'singular_name'         => _x( 'Post Type', 'Post Type Singular Name', 'text_domain' ),
-		'menu_name'             => __( 'Post Types', 'text_domain' ),
-		'name_admin_bar'        => __( 'Post Type', 'text_domain' ),
-		'archives'              => __( 'Item Archives', 'text_domain' ),
-		'attributes'            => __( 'Item Attributes', 'text_domain' ),
-		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
-		'all_items'             => __( 'All Items', 'text_domain' ),
-		'add_new_item'          => __( 'Add New Item', 'text_domain' ),
-		'add_new'               => __( 'Add New', 'text_domain' ),
-		'new_item'              => __( 'New Item', 'text_domain' ),
-		'edit_item'             => __( 'Edit Item', 'text_domain' ),
-		'update_item'           => __( 'Update Item', 'text_domain' ),
-		'view_item'             => __( 'View Item', 'text_domain' ),
-		'view_items'            => __( 'View Items', 'text_domain' ),
-		'search_items'          => __( 'Search Item', 'text_domain' ),
-		'not_found'             => __( 'Not found', 'text_domain' ),
-		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
-		'featured_image'        => __( 'Featured Image', 'text_domain' ),
-		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
-		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
-		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
-		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
-		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
-		'items_list'            => __( 'Items list', 'text_domain' ),
-		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
-		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+		'name'                  => _x( 'Codigos', 'Post Type General Name', 'presencia' ),
+		'singular_name'         => _x( 'Codigo', 'Post Type Singular Name', 'presencia' ),
+		'menu_name'             => __( 'Códigos', 'presencia' ),
+		'name_admin_bar'        => __( 'Códigos', 'presencia' ),
+		'archives'              => __( 'Item Archives', 'presencia' ),
+		'attributes'            => __( 'Item Attributes', 'presencia' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'presencia' ),
+		'all_items'             => __( 'Todos los códigos', 'presencia' ),
+		'add_new_item'          => __( 'Agregar código', 'presencia' ),
+		'add_new'               => __( 'Añadir nuevo', 'presencia' ),
+		'new_item'              => __( 'Nuevo código', 'presencia' ),
+		'edit_item'             => __( 'Editar código', 'presencia' ),
+		'update_item'           => __( 'Actualizar código', 'presencia' ),
+		'view_item'             => __( 'Ver código', 'presencia' ),
+		'view_items'            => __( 'Ver códigos', 'presencia' ),
+		'search_items'          => __( 'Buscar código', 'presencia' ),
+		'not_found'             => __( 'No encontrado', 'presencia' ),
+		'not_found_in_trash'    => __( 'No encontrado en la papelera', 'presencia' ),
+		'featured_image'        => __( 'Imagen destacada', 'presencia' ),
+		'set_featured_image'    => __( 'Establecer imagen destacada', 'presencia' ),
+		'remove_featured_image' => __( 'Remover imagen destacada', 'presencia' ),
+		'use_featured_image'    => __( 'Usar como imagen', 'presencia' ),
+		'insert_into_item'      => __( 'Insetar en código', 'presencia' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'presencia' ),
+		'items_list'            => __( 'Lista de códigos', 'presencia' ),
+		'items_list_navigation' => __( 'Items list navigation', 'presencia' ),
+		'filter_items_list'     => __( 'Filter items list', 'presencia' ),
 	);
 	$args = array(
-		'label'                 => __( 'Post Type', 'text_domain' ),
-		'description'           => __( 'Post Type Description', 'text_domain' ),
+		'label'                 => __( 'Codigo', 'presencia' ),
+		'description'           => __( 'Códigos para GENESIS', 'presencia' ),
 		'labels'                => $labels,
-		'supports'              => array('title', 'editor', 'custom-fields'),
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
 		'taxonomies'            => array( 'category', 'post_tag' ),
 		'hierarchical'          => false,
 		'public'                => true,
-        'show_ui'               => true,
-        'show_in_rest'          => true,
+		'show_ui'               => true,
 		'show_in_menu'          => true,
 		'menu_position'         => 5,
 		'show_in_admin_bar'     => true,
@@ -52,8 +50,7 @@ function custom_post_type() {
 		'publicly_queryable'    => true,
 		'capability_type'       => 'page',
 	);
-	register_post_type( 'post_type', $args );
+	register_post_type( 'codigo', $args );
 
 }
-add_action( 'init', 'custom_post_type', 0 );
-
+add_action( 'init', 'crea_codigo', 0 );
