e3e4b89232c214e8b904c206a31a97cc48fff1c3
Jaime de Greiff <alegreiff@gmail.com>
2019-09-24 16:39:00 -0500

    SX

--- wp-content/themes/genesis-sample/functions/genesis_mods.php
+++ wp-content/themes/genesis-sample/functions/genesis_mods.php
@@ -7,3 +7,14 @@ add_action( 'admin_menu' , 'remove_genesis_post_scripts_box' );
 function remove_genesis_post_scripts_box() {
 remove_meta_box( 'genesis_inpost_scripts_box', 'post', 'normal' ); 
 }
+
+
+//* Do NOT include the opening php tag shown above. Copy the code shown below.
+//* Add custom body class to the head
+add_filter( 'body_class', 'sp_body_class' );
+function sp_body_class( $classes ) {
+	
+	$classes[] = 'custom-classjaime';
+	return $classes;
+	
+}
