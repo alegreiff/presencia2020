09d0d5557730f14215d8bf29db6081fe3c34ed8e
Jaime de Greiff <alegreiff@gmail.com>
2019-09-24 11:04:36 -0500

    Pl

--- wp-content/plugins/blooques-acf/blooques-acf.php
+++ wp-content/plugins/blooques-acf/blooques-acf.php
@@ -22,7 +22,7 @@ function register_acf_block_types() {
         'category'          => 'common',
         'icon'              => 'admin-comments',
         'keywords'          => array( 'testimonial', 'quote' ),
-        'enqueue_style' => get_template_directory_uri() . '/css/bloques/testimonial.css',
+        'enqueue_style'     => get_stylesheet_directory_uri() . '/css/bloques/testimonial.css',
 
         
     ));
