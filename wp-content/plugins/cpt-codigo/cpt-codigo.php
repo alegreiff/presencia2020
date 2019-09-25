ee36a867fd2e0b8d8ec90cd3bda01a22178be727
Jaime de Greiff <alegreiff@gmail.com>
2019-09-25 15:09:38 -0500

    CPT

--- wp-content/plugins/cpt-codigo/cpt-codigo.php
+++ wp-content/plugins/cpt-codigo/cpt-codigo.php
@@ -49,25 +49,6 @@ function crea_codigo() {
 		'label'                 => __( 'Codigo', 'presencia' ),
 		'description'           => __( 'Códigos para GENESIS', 'presencia' ),
 		
-		
-		"public" => true,
-		"publicly_queryable" => true,
-		"show_ui" => true,
-		"delete_with_user" => false,
-		"show_in_rest" => true,
-		"rest_base" => "libros",
-		"rest_controller_class" => "WP_REST_Posts_Controller",
-		"has_archive" => true,
-		"show_in_menu" => true,
-		"show_in_nav_menus" => true,
-		"exclude_from_search" => false,
-		"capability_type" => "post",
-		"map_meta_cap" => true,
-		"hierarchical" => false,
-		"rewrite" => false,
-		"query_var" => true,
-		"menu_position" => 9,
-		"supports" => array( "title", "editor" ),
 	);
 	register_post_type( 'codigo', $args );
 
